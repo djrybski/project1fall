7462f6240b95b27a52a255a66be0e727c7c59b6e
djrybski <31292123+djrybski@users.noreply.github.com>
2018-09-25 21:03:38 -0500

    Update java.js

--- javascript/java.js
+++ javascript/java.js
@@ -1,5 +1,5 @@
 function myFunction() {
-	alert("Thank You For Submitting!");
+	alert("The Science of Baseball has been emailed to you!");
 	window.location.replace("https://djrybski.github.io/project1fall/?fname=d&Email=d%40gmail.com");
 		return false;
 }
